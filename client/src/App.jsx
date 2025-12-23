05e2bca5c60cd6ff76b6ead1bef7ace35a8b91e1
AnanthuNarashimman <ananthu.narashimman@gmail.com>
2025-12-23 14:27:47 +0530

    vercel analytics added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,8 @@ import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
 import './App.css';
 
+import { Analytics } from "@vercel/analytics/react"
+
 import Page from './Pages/Page';
 
 function App() {
@@ -11,6 +13,7 @@ function App() {
   return (
     <>
       <Page />
+      <Analytics />
     </>
   )
 }
